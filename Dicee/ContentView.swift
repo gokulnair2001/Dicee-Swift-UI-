738889b8c6343311a3752fbba9d0c120beb2c860
“gokulnair2001” <gokulnair.2001@gmail.com>
2020-08-14 12:48:56 +0530

    First Commit

--- Dicee/ContentView.swift
+++ Dicee/ContentView.swift
@@ -9,8 +9,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var leftDiceNo = 1
+    @State var rightDiceNo = 1
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        ZStack{
+            Image("background").resizable().edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Image("diceeLogo").frame(width: 100, height: 100, alignment: .center).padding()
+                Spacer()
+                HStack {
+                    
+                    DiceView(n: leftDiceNo)
+                    DiceView(n: rightDiceNo)
+                    
+                }
+                .padding(.horizontal)
+                Spacer()
+                Button(action: {
+                    
+                    self.leftDiceNo = Int.random(in: 1...6)
+                    self.rightDiceNo = Int.random(in: 1...6)
+                    
+                }) {
+                    Text("Roll").font(Font.custom("Avenir", size: 40))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                        .padding(.horizontal)
+                    }.background(Color.red).cornerRadius(10)
+            }
+        }
+    }
+}
+
+
+
+struct DiceView: View {
+    let n:Int
+    var body: some View {
+        
+        Image("dice\(n)").resizable().aspectRatio(1, contentMode: .fit)
+        .padding()
     }
 }
 
@@ -19,3 +61,5 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
