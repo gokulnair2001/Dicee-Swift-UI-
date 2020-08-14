7fc7fa4473e12831d392a650e6428c87eda82220
“gokulnair2001” <gokulnair.2001@gmail.com>
2020-08-14 12:15:12 +0530

    Initial Commit

---
+++ Dicee/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dicee
+//
+//  Created by Gokul Nair on 14/08/20.
+//  Copyright © 2020 Gokul Nair. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
